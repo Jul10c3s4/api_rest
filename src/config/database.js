d3f619e1cdaccec1ffa5bf9aa458cef4332b473f
Julio Cesar <103427020+Jul10c3s4@users.noreply.github.com>
2024-04-19 18:46:55 -0300

    Update database.js

--- src/config/database.js
+++ src/config/database.js
@@ -2,7 +2,7 @@
 require('dotenv').config();
 
 module.exports = {
-  dialect: 'mariadb',
+  dialect: 'mysql',
   host: process.env.DATABASE_HOST,
   port: process.env.DATABASE_PORT,
   username: process.env.DATABASE_USERNAME,
